ec79fbe14f6dbd7fd00b447edd1570a609da9c4b
Simon Coker <kornerstonecoker@gmail.com>
2026-03-23 15:02:23 +0000

    Initial commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformstateXXXXXX"  # paste output value here
+    container_name       = "tfstate"
+    key                  = "hub-spoke/terraform.tfstate"
+    use_azuread_auth     = true  # use Entra ID, not storage key
+  }
+}
---
+++ backend/main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  storage_use_azuread = true
+}
+
+resource "azurerm_resource_group" "tfstate" {
+  name     = "rg-terraform-state"
+  location = "northeurope"
+}
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "stterraformstate${random_string.suffix.result}"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+  # Security Hardening 
+  allow_nested_items_to_be_public = false
+  shared_access_key_enabled = false
+  min_tls_version = "TLS1_2"
+
+  blob_properties {
+    versioning_enabled = true
+
+    delete_retention_policy {
+      days = 30
+    }
+  }
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
+
+resource "random_string" "suffix" {
+  length = 6
+  special = false
+  upper = false
+}
