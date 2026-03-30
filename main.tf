0e89367d2ea401a9b3ebd4144b616726bba17cdc
Simon Coker <kornerstonecoker@gmail.com>
2026-03-30 16:09:08 +0100

    fix: import existing hub resource group into terraform state

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+import {
+  to = module.hub.azurerm_resource_group.hub
+  id = "/subscriptions/716ccd4f-d961-48c5-910f-5ed06af608ad/resourceGroups/rg-connectivity-hub"
+}
+
 # Get the tenant root management group ID
 data "azurerm_management_group" "root" {
   name = data.azurerm_client_config.current.tenant_id
