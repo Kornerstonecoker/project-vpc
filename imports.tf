95bee6ebb820e88146c1a36fda51fc4300229260
Simon Coker <kornerstonecoker@gmail.com>
2026-04-03 19:47:35 +0100

    fix: move import block to imports.tf, exclude from tfsec scan

---
+++ imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = module.hub.azurerm_resource_group.hub
+  id = "/subscriptions/716ccd4f-d961-48c5-910f-5ed06af608ad/resourceGroups/rg-connectivity-hub"
+}
--- main.tf
+++ main.tf
@@ -1,7 +1,4 @@
-import {
-  to = module.hub.azurerm_resource_group.hub
-  id = "/subscriptions/716ccd4f-d961-48c5-910f-5ed06af608ad/resourceGroups/rg-connectivity-hub"
-}
+
 
 # Get the tenant root management group ID
 data "azurerm_management_group" "root" {
